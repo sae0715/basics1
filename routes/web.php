368ad6477cbe2829040755f350ff3ed6c2ca62f9
saeko inamine <saeco0715@gmail.com>
2026-05-17 23:32:07 +0900

    ユーザーコントローラー作成：indexメソッドを実装

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('hello', function () {
     return 'Hello World';
 });
+
+Route::get('/users', [UserController::class, 'index']);
